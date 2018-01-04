490cdf521d86bc38d99bf37070357555ee5a3bad
Attuluri, Srividya (CONT) <Srividya.Attuluri@capitalone.com>
2018-01-04 15:03:09 -0800

    assets files

---
+++ Documents/Udemy/DiceImage/DiceImage/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  DiceImage
+//
+//  Created by Attuluri, Srividya (CONT) on 1/2/18.
+//  Copyright © 2018 Attuluri, Srividya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
